d1ff420fad0872b66bdf949c17cf07186c717003
Mimi <mireiag77@gmail.com>
2024-04-09 19:02:35 +0200

    Feed in progress

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,8 @@
-import Navhead from "./Navhead";
-import data from "../fakedata/fakedata";
-import { useEffect, useState } from "react";
+/* eslint-disable react/prop-types */
 
-function Post() {
-    const [photos, setPhotos] = useState([]);
+
+
+function Post(props) {
 
     const bgcolor = {
 
@@ -11,10 +10,50 @@ function Post() {
         backgroundColor: "#0A141F"
     }
 
+    const textColor = {
+        color: "#CCDBDC",
+    }
+
+    const displayHeader = {
+        display: "flex",
+        justifyContent: "space-between",
+    }
+
+    const display = {
+        display: "flex",
+    }
+
 
     return (
         <div style={bgcolor}>
-            <Navhead />
+            <div className="container mt-5">
+                <div className="post-header" style={displayHeader}>
+                    <div className="post-username">
+                        <p className="username" style={textColor}>{props.username}</p>
+                    </div>
+                    <div className="post-location" style={display}>
+                        <img className="location-icon" src="" />
+                        <p className="location" style={textColor}>{props.location}</p>
+                    </div>
+                </div>
+
+                <img className="post-img" src={props.img} />
+                <div className="post-icons flex">
+                    <div className="insta-icons">
+                        <img className="heart-icon" src="" />
+                        <img className="comment-icon" src="" />
+                    </div>
+                    <div className="encilopedia-icon">
+                        <img className="pedia-icon" src="" />
+                    </div>
+                </div>
+
+                <div className="photo-description">
+                    <p className="description" style={textColor}>{props.comment}</p>
+
+                </div>
+
+            </div>
 
         </div>
     )
