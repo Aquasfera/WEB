08256bdab488b7c4a51decfdaa3edcd2467e8b49
Mimi <mireiag77@gmail.com>
2024-04-12 16:49:26 +0200

    Bugs solved. Data loaded

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import Post from "../components/Post";
 import Navhead from "../components/Navhead";
@@ -5,50 +6,43 @@ import Navhead from "../components/Navhead";
 function Feed() {
 
     const [posts, setPosts] = useState([]);
-    const [locations, setLocations] = useState([]);
 
     useEffect(() => {
-        let idLocation;
         fetch('http://192.168.1.244:3000/api/post')
             .then(response => response.json())
             .then(data => {
                 setPosts(data)
-                posts.forEach(post => {
-                    idLocation = post.location_id;
-                    fetch(`http://192.168.1.244:3000/api/location/${idLocation}`)
-                        .then(response => response.json())
-                        .then(data => setLocations(prevLocations => [...prevLocations, data]))
-                        .catch(error => console.error(error))
-                })
-
-
+                console.log(data)
+                console.log(posts)
             })
             .catch(error => console.error(error))
 
-
-
     }, [])
+
+    if (!posts.length) {
+        return (
+            <div className="spinner-border" role="status">
+                <span >Loading...</span>
+            </div>)
+    }
+
+
 
     return (
         <>
             <Navhead />
-            {posts.map(post => {
-                const location = locations.find(loc => loc.id === post.location_id)
+            {posts.map(cosa => {
+
                 return (
-                    <div key={post.id}>
 
-                        <Post
-                            username={post.user.username}
-                            location={location.name}
-                            img={post.url}
-                            comment={post.comment}
-                            key={locations.id}
-                        />
+                    <Post
+                        username={cosa.user?.username}
+                        location={cosa.location?.name}
+                        img={cosa.url}
+                        comment={cosa.comment}
+                        key={cosa.id}
+                    />
 
-
-
-
-                    </div>
                 );
             })}
         </>
