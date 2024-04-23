47a5f589175ceca1b73b96f4945a1eba2e3609f9
Arnau Orts <orts.brichs.arnau@gmail.com>
2024-04-23 17:26:31 +0200

    href removed y avistamientos implementados

--- src/components/NavheadAquapedia.jsx
+++ src/components/NavheadAquapedia.jsx
@@ -2,6 +2,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 
 const bgcolor = {
   backgroundColor: "#0A141F"
@@ -12,20 +14,20 @@ const nostyle = {
 
 
 function Navhead() {
-    return (
-        <Navbar expand="lg" className='navbar-dark sticky-top' style={bgcolor} navbar-expand-end="true">
-        <Container>
-          <Link to='/' style={nostyle}><Navbar.Brand>Aquasfera</Navbar.Brand></Link>
-          <Navbar.Toggle  aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/aquapedia" >Mapa</Nav.Link>
-              <Nav.Link href="/aquagram" >Instagram</Nav.Link>
-              <Nav.Link href="/aquagram/animals" >Mediterraneo</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    )
+  const navigate = useNavigate()
+  return (
+    <Navbar expand="lg" className='navbar-dark sticky-top' style={bgcolor} navbar-expand-end="true">
+      <Container>
+        <Link to='/' style={nostyle}><Navbar.Brand>Aquasfera</Navbar.Brand></Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link onClick={() => navigate("/aquapedia")}  >Mapa</Nav.Link>
+            <Nav.Link onClick={() => navigate("/aquagram")} >Instagram</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
 }
 export default Navhead;
