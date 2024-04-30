8c1f85f6b53294d559f719b689611b693950c1b7
Adrin63 <adrianriscolopez@gmail.com>
2024-04-30 19:09:35 +0200

    world

--- src/components/NavheadAquagram.jsx
+++ src/components/NavheadAquagram.jsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import PersonalMenu from '../components/PersonalMenu.jsx';
 import { useNavigate } from 'react-router-dom';
+import world from '../assets/icons/feed.svg'
 
 const bgcolor = {
   backgroundColor: "#0A141F"
@@ -15,7 +16,7 @@ function Navhead() {
     <Navbar expand="lg" className='navbar-dark sticky-top pt-1' style={bgcolor} navbar-expand-end="true">
         <Container>
           
-          <Navbar.Brand onClick={() => navigate("/")} >Aquasfera</Navbar.Brand>
+          <Navbar.Brand onClick={() => navigate("/")} ><img src={world} style={{width: '20px', marginBottom: '4px', marginRight: '2px'}}/> Aquasfera</Navbar.Brand>
           <PersonalMenu/>
           {/* {actualUser ? <Navbar.Brand className='text-white' onClick={logout}>{actualUser.username}</Navbar.Brand> : <Navbar.Brand className='text-white'>Nope</Navbar.Brand>} */}
         </Container>
