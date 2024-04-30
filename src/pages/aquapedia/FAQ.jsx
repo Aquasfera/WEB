13ef8e656cbefcc63ec85f7fa2750202cda45b7f
Mimi <mireiag77@gmail.com>
2024-04-30 17:12:50 +0200

    Sea description and FAQ done

--- src/pages/aquapedia/FAQ.jsx
+++ src/pages/aquapedia/FAQ.jsx
@@ -1,46 +1,47 @@
-
+import NavHeadAquapedia from '../../components/NavheadAquapedia';
 
 import "./styles/FAQ.css";
 
 const FAQ = () => {
-  
 
-  
+
+
   const faqData = [
     {
-      question: "¿Por qué lo usamos?",
+      question: "¿Cómo puedo utilizar el mapa interactivo para explorar las diferentes regiones marinas (Mar Mediterráneo, Mar Cantábrico y Océano Atlántico)?",
       answer:
-        "Se utiliza para llenar espacios en el diseño y proporcionar un aspecto preliminar.",
+        "Simplemente haz clic en la región que te interese en el mapa interactivo y se mostrará información sobre las especies marinas que habitan en esa área.",
     },
     {
-      question: "¿Por qué lo usamos?",
+      question: "¿Cómo puedo acceder a las fundaciones que colaboran para conservar el ecosistema marino desde la página?",
       answer:
-        "Se utiliza para llenar espacios en el diseño y proporcionar un aspecto preliminar.",
+        "En la sección de Fundaciones, dentro de la información de cada especie, podrás encontrar enlaces o información sobre las organizaciones que trabajan para conservar el ecosistema marino. Simplemente haz clic en el enlace proporcionado para obtener más detalles sobre su trabajo y cómo puedes apoyarlos.",
     },
     {
-      question: "¿Por qué lo usamos?",
+      question: "¿Necesito estar registrado para poder ver las fotografías que suben los usuarios?",
       answer:
-        "Se utiliza para llenar espacios en el diseño y proporcionar un aspecto preliminar.",
+        "Si quieres acceder a las fotografías mediante Aquagram si necesitarás registrarte. En el caso de que quieras visualizar las imágenes desde Aquapedia no es necesario el registro",
     },
     {
-      question: "¿Por qué lo usamos?",
+      question: "¿Cómo puedo acceder a la información de cada especie?",
       answer:
-        "Se utiliza para llenar espacios en el diseño y proporcionar un aspecto preliminar.",
+        "Dentro de cada área geográfica bastará con hacer clic en la especie de la que quiero obtener más información",
     },
     {
-      question: "¿Por qué lo usamos?",
+      question: "¿Cómo puedo subir una imagen?",
       answer:
-        "Se utiliza para llenar espacios en el diseño y proporcionar un aspecto preliminar.",
+        "Accede al formulario para postear una nueva imagen y selecciona la imagen que quieres postear. Deberás indicar el tipo de especie y la ubicación de la misma.",
     },
     {
-      question: "¿Por qué lo usamos?",
+      question: "¿Cómo puedo contratar al equipo?",
       answer:
-        "Se utiliza para llenar espacios en el diseño y proporcionar un aspecto preliminar.",
+        "En la sección Sobre Nosotros puedes encontrar el contacto (LinkedIn y GitHub) de cada uno de los miembros del equipo",
     },
   ];
 
   return (
     <div className="box0">
+      <NavHeadAquapedia />
       <h2 className="titulo">Preguntas Frecuentes</h2>
 
       {faqData.map((faq, index) => (
